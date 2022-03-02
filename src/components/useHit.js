08818c8684db462b8d883b2efb295b9947dcbd4e
Pratyush Tiwary <cantaloupeinc.1@gmail.com>
2022-03-02 21:08:08 +0530

    Changed backend URL

--- src/components/useHit.js
+++ src/components/useHit.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const hitURL = "https://stocker-be-api.herokuapp.com";
+export const hitURL = "https://stocker.pythonanywhere.com";
 
 export default function useHit(path) {
   async function send(params) {
